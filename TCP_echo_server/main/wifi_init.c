3b9268824bb19ebf2d0534b4e7e5dddfe1803f09
nikidimitrow <nikidimitrow@gmail.com>
2024-06-28 06:35:31 +0300

    comments to explain wifi_init

--- TCP_echo_server/main/wifi_init.c
+++ TCP_echo_server/main/wifi_init.c
@@ -4,25 +4,34 @@
 #include "esp_log.h"
 #include "esp_netif.h"
 
+// Wi-Fi credentials and authentication mode
 #define WIFI_SSID "nikidimitrow"
-#define WIFI_PASS "NobodyWouldUseThis"
+#define WIFI_PASS "NobodyWouldUseThis" // A joke
 #define WIFI_AUTH_MODE WIFI_AUTH_WPA2_PSK
 /* https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-guides/wifi.html */
 
 static const char *TAG = "wifi_init";
 
+// Function to initialize Wi-Fi
 esp_err_t wifi_init(void) 
 {
     
+    // Init the network interface
     ESP_ERROR_CHECK(esp_netif_init());
+    // Default event loop
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     
+    //  Wi-Fi station interface
     esp_netif_create_default_wifi_sta();
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     
+    //Configure Wi-Fi with default
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();   
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    // Wi-Fi mode to station (client)
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     
+    // Set Wi-Fi configuration
     wifi_config_t wifi_config = 
     {
         .sta = 
@@ -32,10 +41,12 @@ esp_err_t wifi_init(void)
             .threshold.authmode = WIFI_AUTH_MODE,
         },
     };
-    
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    //Start the server
     ESP_ERROR_CHECK(esp_wifi_start());
     
+    // Try to connect to the Wi-Fi network
     esp_err_t ret = esp_wifi_connect();
     if (ret != ESP_OK) 
     {
@@ -43,6 +54,7 @@ esp_err_t wifi_init(void)
         return ret;
     }
 
+    //Did we manage to finished fine?
     ESP_LOGI(TAG, "Wi-Fi is done %s", WIFI_SSID);
     return ESP_OK;
 }
